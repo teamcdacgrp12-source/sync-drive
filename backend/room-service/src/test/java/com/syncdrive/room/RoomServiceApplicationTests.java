ebc28219c6a158ceccc33d6dc4b9cd037c5c5f71
shindeharshad553 <shindeharshad553@gmail.com>
2026-07-22 14:27:39 +0530

    test(room): added room service context test

---
+++ backend/room-service/src/test/java/com/syncdrive/room/RoomServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.syncdrive.room;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RoomServiceApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
