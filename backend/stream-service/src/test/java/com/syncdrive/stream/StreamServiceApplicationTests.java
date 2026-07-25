5c7dd94a15a829dd25f2ab6b40c85487941ba176
Pranav Sood <boyp56039@gmail.com>
2026-07-25 16:29:38 +0530

    feat(stream): configure application bootstrap

---
+++ backend/stream-service/src/test/java/com/syncdrive/stream/StreamServiceApplicationTests.java
@@ -0,0 +1,10 @@
+package com.syncdrive.stream;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StreamServiceApplicationTests {
+	@Test
+	void contextLoads() { }
+}
